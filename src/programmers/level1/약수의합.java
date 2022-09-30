8a59e245c8cda36335fe0c05b7876f988bf0be19
seungduru <dlatmddn468@gmail.com>
2022-09-30 20:02:30 +0900

    level1 - 약수의합

---
+++ src/programmers/level1/약수의합.java
@@ -0,0 +1,19 @@
+package programmers.level1;
+
+public class 약수의합 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) answer += i;
+        }
+
+        return answer;
+    }
+}
