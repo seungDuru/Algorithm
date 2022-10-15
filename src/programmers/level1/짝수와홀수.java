b57ce4b2f0774b48b583142442b87f87f251aaa8
seungduru <dlatmddn468@gmail.com>
2022-10-15 14:27:53 +0900

    level1 - 짝수와 홀수

---
+++ src/programmers/level1/짝수와홀수.java
@@ -0,0 +1,13 @@
+package programmers.level1;
+
+public class 짝수와홀수 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public String solution(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
+}
