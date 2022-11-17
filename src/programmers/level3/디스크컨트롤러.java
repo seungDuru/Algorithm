c8f9a5f14fbe3fc0d032039ab4555bf7b1497591
seungduru <dlatmddn468@gmail.com>
2022-11-17 18:25:05 +0900

    level2 - 디스크 컨트롤러

---
+++ src/programmers/level3/디스크컨트롤러.java
@@ -0,0 +1,21 @@
+package programmers.level3;
+
+public class 디스크컨트롤러 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        String[] arr = "211020101011".split("0");
+
+        for(String s :arr) {
+            System.out.println(s);
+        }
+    }
+
+    public int solution(int[][] jobs) {
+
+
+        int answer = 0;
+        return answer;
+    }
+}
