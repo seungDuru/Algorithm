143c299b7160e4b91669512247c1191911be3b65
seungduru <dlatmddn468@gmail.com>
2022-11-28 15:19:46 +0900

    level2 - 수식 최대화

---
+++ src/programmers/level2/수식최대화.java
@@ -0,0 +1,86 @@
+package programmers.level2;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class 수식최대화 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+
+        System.out.println(solution("100-200*300-500+20"));
+
+    }
+
+    static String expression;
+    static long max = 0;
+
+    public static long solution(String expression_word) {
+        expression = expression_word;
+        permutation(0, 3, new String[]{"+", "-", "*"}, new boolean[3], "");
+        return max;
+    }
+
+    public static void permutation(int startDept, int targetDepth, String[] arr, boolean[] check, String result) {
+        if (startDept == targetDepth) {
+            calc(result);
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    permutation(startDept + 1, targetDepth, arr, check, result + arr[i]);
+                    check[i] = false;
+                }
+            }
+        }
+    }
+
+    public static void calc(String operator) {
+        StringBuilder post = new StringBuilder();
+        List<Long> operandList = new LinkedList<>();
+        List<String> operatorList = new LinkedList<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*') {
+                operandList.add(Long.parseLong(post.toString()));
+                post = new StringBuilder();
+                operatorList.add(String.valueOf(expression.charAt(i)));
+            } else
+                post.append(expression.charAt(i));
+        }
+
+        operandList.add(Long.parseLong(post.toString()));
+
+        for (int i = 0; i < 3; i++) {
+            String nowOperator = String.valueOf(operator.charAt(i));
+
+            while(operatorList.size() != 0){
+                int index = operatorList.indexOf(nowOperator);
+
+                if(index == -1)
+                    break;
+                else{
+                    switch (nowOperator){
+                        case "+":
+                            operandList.add(index, operandList.get(index) + operandList.get(index + 1));
+                            break;
+                        case "-":
+                            operandList.add(index, operandList.get(index) - operandList.get(index + 1));
+                            break;
+                        case "*":
+                            operandList.add(index, operandList.get(index) * operandList.get(index + 1));
+                            break;
+                    }
+
+                    operandList.remove(index + 1);
+                    operandList.remove(index + 1);
+
+                    operatorList.remove(index);
+                }
+            }
+        }
+
+        max = Math.max(max, Math.abs(operandList.get(0)));
+    }
+}
